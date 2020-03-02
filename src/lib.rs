6eb4c34f44b085e74ede6a3b3f37992106d868d3
RobbieClarken <robbie.clarken@gmail.com>
2020-03-03 01:12:41 +1030

    Move mod_b into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,15 @@ mod mod_a {
     pub(crate) use macros::xyz;
 }
 
-mod mod_b;
+mod mod_b {
+    mod xyz {}
+
+    use crate::mod_a::*;
+
+    mod mod_c {
+        #[allow(dead_code,unused_imports)]
+        fn foobar() {
+            use super::xyz;
+        }
+    }
+}
